df93a17e061898de37e32b2de289183f13e12dab
albedamm <s215067@dtu.dk>
2024-09-20 13:27:57 +0200

    lots of changes

--- docs/tutorial/tutorial.py
+++ docs/tutorial/tutorial.py
@@ -3,6 +3,7 @@
 
 # %% 
 import proteusAI as pai
+import pandas as pd
 pai.__version__
 
 # # MLDE Tutorial
@@ -11,7 +12,7 @@ pai.__version__
 # The data must contain the mutant sequences and y_values for the MLDE workflow. It is recommended to have useful sequence names for later interpretability, and to define the data type.
 
 # %% 
-library = pai.Library(source='demo/demo_data/master_dataset.csv', seqs_col='binder_seq', y_col='pae_interaction_HLA-A101-RVTDESILSY', 
+library = pai.Library(source='demo/demo_data/ogTarget_dataset.csv', seqs_col='binder_seq', y_col='pae_interaction_HLA-A101-RVTDESILSY', 
                     y_type='num', names_col='binder_name')
 
 
@@ -33,7 +34,7 @@ model = pai.Model(library=library)
 # ## Training the model
 
 # %% 
-_ = model.train(k_folds=5, model_type='rf', x='blosum62')
+_ = model.train(k_folds=5, model_type='rf', x='blosum62', seed=42)
 
 
 # %% [markdown]
@@ -41,5 +42,8 @@ _ = model.train(k_folds=5, model_type='rf', x='blosum62')
 # Searching new mutants will produce an output dataframe containing the new predictions. Here we are using the expected improvement ('ei') acquisition function.
 
 # %% 
-out = model.search(acq_fn='ei', optim_problem='min')
-out
+out = model.search(optim_problem='max', overwrite=True)
+out.to_csv('demo/demo_data/predictions.csv')
+
+
+# acq_fn='ei'
