071f92cd08e5b513621298473157cdfef699d7a8
albedamm <s215067@dtu.dk>
2024-09-24 12:41:40 +0200

    Changed dataset

--- docs/tutorial/tutorial.py
+++ docs/tutorial/tutorial.py
@@ -12,7 +12,7 @@ pai.__version__
 # The data must contain the mutant sequences and y_values for the MLDE workflow. It is recommended to have useful sequence names for later interpretability, and to define the data type.
 
 # %% 
-library = pai.Library(source='demo/demo_data/ogTarget_dataset.csv', seqs_col='binder_seq', y_col='pae_interaction_HLA-A101-RVTDESILSY', 
+library = pai.Library(source='demo/demo_data/master_dataset.csv', seqs_col='binder_seq', y_col='pae_interaction_HLA-A101-RVTDESILSY', 
                     y_type='num', names_col='binder_name')
 
 
