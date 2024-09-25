3544fa662effbd42c5e040ea0beb8edeb7368491
Mathilde Due <s215063@dtu.dk>
2024-09-25 12:30:17 +0200

    Removed spaces

--- src/proteusAI/Model/model.py
+++ src/proteusAI/Model/model.py
@@ -30,7 +30,6 @@ import pandas as pd
 import gpytorch
 import numpy as np
 
-
 class Model:
     """
     The Model object allows the user to create machine learning models, using 
@@ -870,7 +869,6 @@ class Model:
             return out
 
         
-
     def _class_search(self, N=10, optim_problem='max', labels=['all'], method='ga', max_eval=10000, pbar=None):
         """
         Sample diverse sequences and return a mask with 1 for selected indices and 0 for non-selected.
